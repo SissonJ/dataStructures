6e9e2bb8acc549d4569e773147b881a75fd5af0a
jackb <jackb@140.88.203.140>
2020-12-05 11:02:56 -0600

    Working on Gui

--- DSFinalProject/src/module-info.java
+++ DSFinalProject/src/module-info.java
@@ -2,4 +2,7 @@ module DSFinalProject
 {
 	requires javafx.graphics;
 	requires javafx.base;
+	requires javafx.controls;
+	
+	opens morabaraba to javafx.graphics;
 }
--- DSFinalProject/src/morabaraba/Gui.java
+++ DSFinalProject/src/morabaraba/Gui.java
@@ -3,17 +3,48 @@ package morabaraba;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Gui extends Application implements EventHandler<ActionEvent>
 {
+	
+	private Button[][] gameBoardB;
+	
+	private Button resetB;
+	
+	private Label messageL;
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
 
 	@Override
-	public void start(Stage arg0) throws Exception
+	public void start(Stage primaryStage) throws Exception
 	{
-		// TODO Auto-generated method stub
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 625, 500);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Morabaraba");
 		
-	}
+		gameBoardB = new Button[3][8];
+		
+		messageL = new Label();
+		
+		resetB = new Button("Reset");
+		resetB.setOnAction(this);
+		
+		root.setTop(resetB);
+		
+		root.setBottom(messageL);
+		
+		primaryStage.show();
+		}
 
 	@Override
 	public void handle(ActionEvent arg0)
