06acf93543b6db49eb9b59dd095203f57faed495
jackb <jackb@140.88.204.53>
2020-12-09 15:12:19 -0600

    Cows can't be killed in a mill

--- DSFinalProject/src/morabaraba/Controler.java
+++ DSFinalProject/src/morabaraba/Controler.java
@@ -12,11 +12,13 @@ public class Controler
 	/** phase two and three holder variable for the last space chosen */
 	private int[] curSpace;
 
+	// private int[] endGameCurSpace;
+
 	/**
 	 * boolean that keeps track if the next click should kill a cow; false = no
 	 * kill, true = kill
 	 */
-	private Boolean killCow;
+	private boolean killCow;
 
 	/** 3 by 8 int array to keep track of the state of the gameboard */
 	private int[][] gameBoard;
@@ -25,7 +27,7 @@ public class Controler
 	private String message;
 
 	/** keeps track if this click is the first or second for phase two and three */
-	private Boolean secondClick;
+	private boolean secondClick;
 
 	/** holds which player is next; 1 is red, 2 is blue */
 	private int player;
@@ -43,18 +45,23 @@ public class Controler
 
 	public void buttonPress(int x, int y) throws IllegalArgumentException
 	{
+		if (endGame())
+		{
+			throw new IllegalArgumentException("Game is over; please reset");
+		}
 		if (killCow)
 		{
 			shootCow(x, y);
 			killCow = !killCow;
 			player = player == 1 ? 2 : 1;
+			message = "Player " + player + "s turn!";
 			return;
 		}
 		if (turns != 0)
 		{
 			if (gameBoard[x][y] == 0)
 			{
-				phaseOne(x, y);
+				gameBoard[x][y] = player;
 
 				turns--;
 				if (player == 1)
@@ -67,6 +74,33 @@ public class Controler
 						"This Spot is owned by Player " + gameBoard[x][y] + "! Please choose a new location");
 			}
 
+		} else
+		{
+			message = "Select a cow to move!";
+			if (!secondClick)
+			{ //TODO make sure this click has an adjacent spot
+				if (gameBoard[x][y] != player)
+				{
+					throw new IllegalArgumentException(
+							"This Spot is owned by Player " + gameBoard[x][y] + "! Please choose a new location");
+				}
+				curSpace[0] = x;
+				curSpace[1] = y;
+				secondClick = !secondClick;
+				message = "Select a spot to move to!";
+				return;
+			} else
+			{
+				if (gameBoard[x][y] == 0)
+				{
+					phaseTwoAndThree(x, y);
+					secondClick = !secondClick;
+				} else
+				{
+					throw new IllegalArgumentException(
+							"This Spot is owned by Player " + gameBoard[x][y] + "! Please choose an empty location");
+				}
+			}
 		}
 		if (isMill(x, y))
 		{
@@ -76,23 +110,39 @@ public class Controler
 			return;
 		}
 		player = player == 1 ? 2 : 1;
-	}
-
-	private void phaseOne(int x, int y)
-	{
-		gameBoard[x][y] = player;
-
 		message = "Player " + player + "s turn!";
-
+		endGame(); //TODO Message didn't update when 3rd to last cow was killed
 	}
 
 	private void phaseTwoAndThree(int x, int y) throws IllegalArgumentException
 	{
-
+		if ((player == 1 && cowCountRed > 3) || (player == 2 && cowCountBlue > 3))
+		{
+			if (!isAdjacent(curSpace, x, y))
+				throw new IllegalArgumentException("That space is not adjacent to the cow you chose!");
+		}
+		gameBoard[x][y] = player;
+		gameBoard[curSpace[0]][curSpace[1]] = 0;
 	}
 
-	private boolean isAdjacent(int curSpaceX, int curSpaceY, int newSpaceX, int newSpaceY)
+	private boolean isAdjacent(int[] oldSpace, int newSpaceX, int newSpaceY)
 	{
+		// TODO remove psudoCode
+		/*
+		 * If curX == newX If |curY-newY| == 1 return true Else |curY-newY| == 7 return
+		 * true Else If curY == newY && curX -newX return true Return false
+		 */
+		if (oldSpace[0] == newSpaceX)
+		{
+			if (Math.abs(oldSpace[1] - newSpaceY) == 1)
+				return true;
+			else if (Math.abs(oldSpace[1] - newSpaceY) == 7)
+				return true;
+		} else
+		{
+			if (oldSpace[1] == newSpaceY && Math.abs(oldSpace[0] - newSpaceX) == 1)
+				return true;
+		}
 		return false;
 	}
 
@@ -118,16 +168,20 @@ public class Controler
 
 	private boolean millCaseTwo(int x, int y)
 	{
-		if ((y==1||y==2||y==0)&&gameBoard[x][0] == player && gameBoard[x][1] == player && gameBoard[x][2] == player)
+		if ((y == 1 || y == 2 || y == 0) && gameBoard[x][0] == player && gameBoard[x][1] == player
+				&& gameBoard[x][2] == player)
 		{
 			return true;
-		} else if ((y==6||y==5||y==4)&&gameBoard[x][6] == player && gameBoard[x][5] == player && gameBoard[x][4] == player)
+		} else if ((y == 6 || y == 5 || y == 4) && gameBoard[x][6] == player && gameBoard[x][5] == player
+				&& gameBoard[x][4] == player)
 		{
 			return true;
-		} else if ((y==2||y==3||y==4)&&gameBoard[x][2] == player && gameBoard[x][3] == player && gameBoard[x][4] == player)
+		} else if ((y == 2 || y == 3 || y == 4) && gameBoard[x][2] == player && gameBoard[x][3] == player
+				&& gameBoard[x][4] == player)
 		{
 			return true;
-		} else if ((y==0||y==7||y==6)&&gameBoard[x][0] == player && gameBoard[x][7] == player && gameBoard[x][6] == player)
+		} else if ((y == 0 || y == 7 || y == 6) && gameBoard[x][0] == player && gameBoard[x][7] == player
+				&& gameBoard[x][6] == player)
 		{
 			return true;
 		}
@@ -139,24 +193,87 @@ public class Controler
 		// if either player is down to 2 cows
 		// if both players still have 12 cows after phase 1
 		// if either player has no legal moves... ie in phase 2 but w/o adjacent space
+
+		boolean redAdj = false;
+
+		boolean blueAdj = false;
+
+		if (turns == 0)
+		{
+			if (cowCountBlue < 3)
+			{
+				message = "RED WINS!!!";
+				return true;
+			}
+			if (cowCountRed < 3)
+			{
+				message = "BLUE WINS!!!";
+				return true;
+			}
+			if (cowCountRed == 12 && cowCountBlue == 12)
+			{
+				message = "Draw :(";
+				return true;
+			}
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 8; j++)
+				{
+					if (gameBoard[i][j] == 1 && hasAdjacent(i, j))
+					{
+						redAdj = true;
+					} else if (gameBoard[i][j] == 2 && hasAdjacent(i, j))
+					{
+						blueAdj = true;
+					}
+				}
+			}
+			if (!redAdj)
+			{
+				message = "BLUE WINS!!!";
+				return true;
+			}
+			if (!blueAdj)
+			{
+				message = "RED WINS!!!";
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean hasAdjacent(int x, int y)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				if (isAdjacent(new int[]
+				{ x, y }, i, j) && gameBoard[i][j] == 0)
+				{
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 
 	private void shootCow(int x, int y) throws IllegalArgumentException
 	{
-		// TODO
-		if(gameBoard[x][y] == 0 || gameBoard[x][y] == player)
+		// Make sure cows in mills can't be killed minus the exception case
+		if (gameBoard[x][y] == 0 || gameBoard[x][y] == player)
 		{
 			throw new IllegalArgumentException("Can't kill an empty space or your own cow!");
 		}
 		boolean temp = false;
-		if(isMill(x,y))
+		player = player == 1 ? 2 : 1;
+		if (isMill(x, y))
 		{
-			for(int i = 0; i<3; i++)
+			for (int i = 0; i < 3; i++)
 			{
-				for(int j = 0; j<8; j++)
+				for (int j = 0; j < 8; j++)
 				{
-					if(!isMill(i,j)&&gameBoard[i][j]==gameBoard[x][y])
+					if (!isMill(i, j) && gameBoard[i][j] == gameBoard[x][y])
 					{
 						temp = true;
 						break;
@@ -164,9 +281,11 @@ public class Controler
 				}
 			}
 		}
-		if(temp)
+		player = player == 1 ? 2 : 1;
+		if (temp)
 			throw new IllegalArgumentException("Can't kill cows in a mill");
-		gameBoard[x][y]=0;
+		gameBoard[x][y] = 0;
+		
 		if (player == 2)
 			cowCountRed--;
 		else
@@ -180,7 +299,7 @@ public class Controler
 
 	public void reset()
 	{
-		turns = 24;
+		turns = 16;
 		cowCountRed = 0;
 		cowCountBlue = 0;
 		killCow = false;
